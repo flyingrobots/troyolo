afa7cb548e1223f1e79c6d0470dba3eace7c9211
James Ross <jkross+git@gmail.com>
2012-10-18 23:14:36 -0700

    Adds search queries, ignore list, save data file, etc.

--- troyolo.rb
+++ troyolo.rb
@@ -5,59 +5,79 @@ require 'twitter'
 require 'oj'
 
 CONFIG_FILE = "./troyolo_config.json"
+SAVE_FILE = "./troyolo_save.json"
 
-puts "AUTOMATED TROYOLO DROP"
+$config = Oj.load_file(File.expand_path(CONFIG_FILE))
+$saveData = Oj.load_file(File.expand_path(SAVE_FILE))
 
-#///////////////////////////////////////////////////////////////////////////////
-# open and read config file
-#///////////////////////////////////////////////////////////////////////////////
+$twitterConfig = $config["twitter_config"]
+$dropURLs = $config["drops"]
+$quips = $config["quips"]
+$ignoreList = $config["twitter_users_to_ignore"]
+$searchSize = $config["search_size"]
 
-config = Oj.load_file(File.expand_path(CONFIG_FILE))
+puts "AUTOMATED DROP DELIVERY"
+puts "Upcoming queries..."
+p $saveData
 
-#///////////////////////////////////////////////////////////////////////////////
-# authenticate w/twitter
-#///////////////////////////////////////////////////////////////////////////////
+#-------------------------------------------------------------------------------
+def searchPublicTweets(query, lastTweetIdSeenForThisQuery)
+	puts ""
+	puts "Last tweet id seen for query (#{query}): #{lastTweetIdSeenForThisQuery}"
+	searchResults = Twitter.search(
+			"#troyolo", 
+			:count => $searchSize, 
+			:result_type => "recent",
+			:since_id => lastTweetIdSeenForThisQuery
+		)
+	puts "Found #{searchResults.statuses.count} new tweets."
+	puts "Last tweet found: #{searchResults.max_id}"
+	return searchResults
+end
 
-twitterConfig = config["twitter_config"]
+#-------------------------------------------------------------------------------
+def deliverTheDrop(tweet)
+	if $ignoreList.index(tweet.from_user)
+		puts "Ignoring tweet from user: #{tweet.from_user}"
+	else
+		drop = $dropURLs[rand($dropURLs.count)]
+		quip = $quips[rand($quips.count)]
+		puts "#{tweet.from_user}: #{tweet.text}"
+		puts "\tSending drop (#{drop}) to #{tweet.from_user} in response to (#{tweet.id})"
+		dropTweet = "@#{tweet.from_user} #{quip} #{drop} [secret code: #{rand(tweet.id)}]"
+		puts "\t#{dropTweet}"
+		Twitter.update("#{dropTweet}", :in_reply_to_status_id => tweet.id)
+	end
+end
 
-Twitter.configure do |t|
-	t.consumer_key = twitterConfig["consumer_key"]
-	t.consumer_secret = twitterConfig["consumer_secret"]
-	t.oauth_token = twitterConfig["oauth_token"]
-	t.oauth_token_secret = twitterConfig["oauth_token_secret"]
+#-------------------------------------------------------------------------------
+def searchAndDeliver(query)
+	lastTweetIdSeenForThisQuery = $saveData[query]
+	lastTweetIdSeenForThisQuery = 0 if lastTweetIdSeenForThisQuery.nil?
+	searchResults = searchPublicTweets(query, lastTweetIdSeenForThisQuery)
+	searchResults.statuses.each() { |tweet|	
+		deliverTheDrop(tweet)
+	}
+	$saveData[query] = searchResults.max_id
 end
 
 #///////////////////////////////////////////////////////////////////////////////
-# search for tweets containing #troyolo
+# deliver the drop
 #///////////////////////////////////////////////////////////////////////////////
 
-puts "Last known tweet id: #{config["last_search_result_id"]}"
+# login to twitter
+Twitter.configure do |t|
+	t.consumer_key = $twitterConfig["consumer_key"]
+	t.consumer_secret = $twitterConfig["consumer_secret"]
+	t.oauth_token = $twitterConfig["oauth_token"]
+	t.oauth_token_secret = $twitterConfig["oauth_token_secret"]
+end
 
-searchResults = Twitter.search(
-		"#troyolo", 
-		:count => config["max_drops_per_delivery"], 
-		:result_type => "recent",
-		:since_id => config["last_search_result_id"]
-	)
-
-puts "Found #{searchResults.statuses.count} tweets."
-
-#///////////////////////////////////////////////////////////////////////////////
-# deliver the drop in response to each search result
-#///////////////////////////////////////////////////////////////////////////////
-
-dropURLs = config["drops"]
-quips = config["quips"]
-searchResults.statuses.each() { |tweet|
-	drop = dropURLs[rand(dropURLs.count)]
-	quip = quips[rand(quips.count)]
-	puts "#{tweet.from_user}: #{tweet.text}"
-	puts "\tSending drop (#{drop}) to #{tweet.from_user} in response to (#{tweet.id})"
-	dropTweet = "@#{tweet.from_user} #{quip} #{drop} [secret code: #{rand(tweet.id)}]"
-	puts "\t#{dropTweet}"
-	Twitter.update("#{dropTweet}", :in_reply_to_status_id => tweet.id)
+# search and deliver
+searches = $config["searches"]
+searches.each() { |query|
+	searchAndDeliver(query)
 }
 
-puts "Last tweet found: #{searchResults.max_id}"
-config["last_search_result_id"] = searchResults.max_id
-Oj.to_file(CONFIG_FILE, config, :pretty => true)
+# save query info
+Oj.to_file(SAVE_FILE, $saveData, :indent => 4)
